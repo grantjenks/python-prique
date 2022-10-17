c45b85c3ad110dbc481df462988c0147d49ef3fb
Grant Jenks <grant.jenks@gmail.com>
2022-10-16 22:30:12 -0700

    Thinking out loud

--- src/prique/_core.pxd
+++ src/prique/_core.pxd
@@ -1,2 +1,30 @@
+import cython
+
+
+cdef int insort(list keys, object key)
+
+
+cdef class Branch:
+    cdef Branch _parent
+    cdef int _total
+    cdef object _max
+    cdef object _left
+    cdef object _right
+
+
+cdef class Leaf(Branch):
+    cdef list _keys
+    cdef list _values
+
+
 cdef class Prique:
-    pass
+    cdef Branch _tree
+
+    cdef init(self)
+
+    @cython.locals(branch=Branch, leaf=Leaf)
+    cdef int add(self, object key, object value)
+
+    cdef int discard(self, object key, object value)
+
+    cdef int len(self)
--- src/prique/_core.py
+++ src/prique/_core.py
@@ -1,2 +1,89 @@
+"""Prique Core
+"""
+
+
+def insort(keys, key):
+    return 0
+
+
+
 class Prique:
+    def init(self):
+        self._tree = None
+
+
+    def add(self, key, value):
+        if self._tree is None:
+            branch = Branch()
+            leaf = Leaf()
+
+            branch._parent = self
+            branch._total = 1
+            branch._max = key
+            branch._left = leaf
+            branch._right = None
+
+            leaf._parent = branch
+            leaf._total = 1
+            leaf._left = None
+            leaf._right = None
+            leaf._keys = [key]
+            leaf._values = [value]
+
+            self._tree = branch
+            return 0
+
+        branch = self._tree
+
+        while type(branch) is not Leaf:
+            if branch._left is None:
+                branch = branch._right
+            elif branch._right is None:
+                branch = branch._left
+            elif key < branch._left._max:
+                branch = branch._left
+            else:
+                branch = branch._right
+
+        leaf = branch
+
+        if leaf._total < 33:
+            index = insort(leaf._keys, key)
+            leaf._values.insert(index, value)
+            leaf._total += 1
+            if index == leaf._total:
+                leaf._max = key
+            branch = leaf._parent
+            while branch is not None:
+                branch._total += 1
+                branch = branch._parent
+                # TODO: Might this require rebalancing?
+
+
+    def discard(self, key, value):
+        if self._tree is None:
+            return False
+        pass  # TODO
+
+
+    def len(self):
+        return self._tree._total
+
+
+class Branch:
     pass
+    # _parent = None
+    # _total = 0
+    # _max = None
+    # _left = None
+    # _right = None
+
+
+class Leaf:
+    pass
+    # _parent = None
+    # _count = 0
+    # _next = None
+    # _prev = None
+    # _keys = [None] * MAX_NODE_SIZE
+    # _values = [None] * MAX_NODE_SIZE
