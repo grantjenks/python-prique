9a95b9b78093f13f4005654ab95492dcbaa46d3c
Grant Jenks <grant.jenks@gmail.com>
2022-10-17 23:14:30 -0700

    Add test for cython

--- tests/test_core_prique.py
+++ tests/test_core_prique.py
@@ -1,6 +1,11 @@
 from prique.core import Prique
 
 
+def test_cython():
+    from prique import core
+    assert not core.__file__.endswith('.py')
+
+
 def test_init():
     p = Prique()
     p.init()
