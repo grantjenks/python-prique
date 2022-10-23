e549e79f303928df5f502d19b0a11cd178372fba
Grant Jenks <grant.jenks@gmail.com>
2022-10-22 22:24:16 -0700

    More tests

--- tests/test_core_prique.py
+++ tests/test_core_prique.py
@@ -1,4 +1,6 @@
+import collections
 import itertools
+import random
 import string
 
 from prique.core import Prique
@@ -13,6 +15,7 @@ def test_init():
     p = Prique()
     p.init()
     assert p.check() == 0
+    assert p.len() == 0
 
 
 def test_add():
@@ -20,6 +23,7 @@ def test_add():
     p.init()
     p.add_left(4, 'd')
     assert p.check() == 0
+    assert p.len() == 1
 
 
 def test_add_inc():
@@ -29,7 +33,7 @@ def test_add_inc():
     for index, letter in zip(range(1000), letters):
         p.add_left(index, letter)
         assert p.check() == 0
-    p.dot()
+    assert p.len() == 1000
 
 
 def test_add_dec():
@@ -39,4 +43,38 @@ def test_add_dec():
     for index, letter in zip(range(0, -1000, -1), letters):
         p.add_left(index, letter)
         assert p.check() == 0
-    p.dot()
+    assert p.len() == 1000
+
+
+def test_add_rand():
+    p = Prique()
+    p.init()
+    rand = random.Random(0)
+    letters = itertools.repeat(string.ascii_lowercase)
+    items = list(zip(range(1000), letters))
+    rand.shuffle(items)
+    for index, letter in items:
+        p.add_left(index, letter)
+        assert p.check() == 0
+    assert p.len() == 1000
+
+
+def test_add_same():
+    p = Prique()
+    p.init()
+    for i in range(1000):
+        p.add_left(0, 'a')
+        p.check()
+    assert p.len() == 1000
+
+
+def test_add_new_max():
+    p = Prique()
+    p.init()
+    for i in range(100):
+        p.add_left(i * 2, 'a')
+        p.check()
+    for i in range(100):
+        p.add_left(81, 'b')
+        p.check()
+    assert p.len() == 200
